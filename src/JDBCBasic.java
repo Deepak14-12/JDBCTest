0d9e45ed0b3a88c45626f4d9a54ff24b28fab67f
Deepak14-12 <bisen1412@gmail.com>
2025-06-16 01:26:00 +0530

    Initial commit

--- src/JDBCBasic.java
+++ src/JDBCBasic.java
@@ -1,6 +1,6 @@
 import java.sql.*;
 public class JDBCBasic {
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3306/jdbc";
         String username = "root";
         String password = "root";
