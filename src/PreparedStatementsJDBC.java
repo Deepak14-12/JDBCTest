7a0d6e5649fc8a3d7d61c91260fea4c43e9b7fd2
Deepak14-12 <bisen1412@gmail.com>
2025-06-17 01:39:41 +0530

    updated "PreparedStatementsJDBC"

--- src/PreparedStatementsJDBC.java
+++ src/PreparedStatementsJDBC.java
@@ -3,6 +3,7 @@ import java.sql.SQLException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Scanner;
 
 
 public class PreparedStatementsJDBC {
@@ -24,19 +25,28 @@ public class PreparedStatementsJDBC {
         try {//establish connection
             Connection connection = DriverManager.getConnection(url, username, password);
             System.out.println("connection established successfully.");
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter Name: ");
+            String name = sc.nextLine();
+            System.out.println("Enter Job: ");
+            String job = sc.nextLine();
             //Statement statement = connection.createStatement();
 
             //prepared statements 1 (retrieval of data)
             PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setString(1, "deepak");
-            preparedStatement.setString(2, "intern");
+            /*preparedStatement.setString(1, "deepak");
+              preparedStatement.setString(2, "intern");
+            */
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, job);
 
             ResultSet resultSet = preparedStatement.executeQuery();
 
             while (resultSet.next()) {
                 int id = resultSet.getInt("id");
-                String name = resultSet.getString("name");
-                String job = resultSet.getString("job");
+                 name = resultSet.getString("name");
+                 job = resultSet.getString("job");
                 double salary = resultSet.getDouble("salary");
                 System.out.println("ID: " + id);
                 System.out.println("NAME: " + name);
@@ -44,12 +54,29 @@ public class PreparedStatementsJDBC {
                 System.out.println("SALARY: " + salary);
             }
 
+            System.out.println("Enter ID: ");
+            int id = sc.nextInt();
+            sc.nextLine();
+            System.out.println("Enter Name: ");
+            name = sc.nextLine();
+            System.out.println("Enter Job: ");
+            job = sc.nextLine();
+            System.out.println("Enter Salary: ");
+            double salary = sc.nextDouble();
+            /*
             //prepared statements 2 (insertion of data)
             preparedStatement = connection.prepareStatement(query1);
-            preparedStatement.setInt(1, 4);
+            preparedStatement.setInt(1, id);
             preparedStatement.setString(2, "rohit");
             preparedStatement.setString(3, "farming");
             preparedStatement.setDouble(4, 45265.25);
+            */
+            //prepared statements 2 (insertion of data)
+            preparedStatement = connection.prepareStatement(query1);
+            preparedStatement.setInt(1, id);
+            preparedStatement.setString(2, name);
+            preparedStatement.setString(3, job);
+            preparedStatement.setDouble(4, salary);
 
             int rowsAffected = preparedStatement.executeUpdate();
             preparedStatement = connection.prepareStatement(query1);
